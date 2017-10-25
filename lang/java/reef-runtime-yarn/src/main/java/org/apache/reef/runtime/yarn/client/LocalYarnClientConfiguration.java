5ced5b37681c43d7efe4d18101c5333784c48ef6
Sean Po <sepo@microsoft.com>
2017-10-25 15:42:29 -0700

    Fixed some checkstyle issues preventing push

--- lang/java/reef-runtime-yarn/src/main/java/org/apache/reef/runtime/yarn/client/LocalYarnClientConfiguration.java
+++ lang/java/reef-runtime-yarn/src/main/java/org/apache/reef/runtime/yarn/client/LocalYarnClientConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.reef.runtime.yarn.client;
 
 import org.apache.reef.annotations.audience.ClientSide;
@@ -23,22 +41,22 @@ import org.apache.reef.util.logging.LoggingSetup;
 @ClientSide
 public class LocalYarnClientConfiguration extends ConfigurationModuleBuilder {
 
-    static {
-        LoggingSetup.setupCommonsLogging();
-    }
+  static {
+    LoggingSetup.setupCommonsLogging();
+  }
 
-    public static final OptionalParameter<String> ROOT_FOLDER = new OptionalParameter<>();
+  public static final OptionalParameter<String> ROOT_FOLDER = new OptionalParameter<>();
 
-    public static final ConfigurationModule CONF = new LocalYarnClientConfiguration()
-            .merge(CommonRuntimeConfiguration.CONF)
-            // Bind YARN-specific classes
-            .bindImplementation(JobSubmissionHandler.class, LocalYarnJobSubmissionHandler.class)
-            .bindImplementation(DriverConfigurationProvider.class, YarnDriverConfigurationProviderImpl.class)
-            .bindImplementation(RuntimeClasspathProvider.class, YarnClasspathProvider.class)
-            .bindImplementation(UserCredentials.class, YarnProxyUser.class)
-            // Bind the parameters given by the user
-            // Bind external constructors. Taken from  YarnExternalConstructors.registerClientConstructors
-            .bindConstructor(org.apache.hadoop.yarn.conf.YarnConfiguration.class, YarnConfigurationConstructor.class)
-            .bindNamedParameter(RootFolder.class, ROOT_FOLDER)
-            .build();
+  public static final ConfigurationModule CONF = new LocalYarnClientConfiguration()
+      .merge(CommonRuntimeConfiguration.CONF)
+      // Bind YARN-specific classes
+      .bindImplementation(JobSubmissionHandler.class, LocalYarnJobSubmissionHandler.class)
+      .bindImplementation(DriverConfigurationProvider.class, YarnDriverConfigurationProviderImpl.class)
+      .bindImplementation(RuntimeClasspathProvider.class, YarnClasspathProvider.class)
+      .bindImplementation(UserCredentials.class, YarnProxyUser.class)
+      // Bind the parameters given by the user
+      // Bind external constructors. Taken from  YarnExternalConstructors.registerClientConstructors
+      .bindConstructor(org.apache.hadoop.yarn.conf.YarnConfiguration.class, YarnConfigurationConstructor.class)
+      .bindNamedParameter(RootFolder.class, ROOT_FOLDER)
+      .build();
 }
--- lang/java/reef-runtime-yarn/src/test/java/org/apache/reef/runtime/yarn/client/TestLocalYarnClientConfiguration.java
+++ lang/java/reef-runtime-yarn/src/test/java/org/apache/reef/runtime/yarn/client/TestLocalYarnClientConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.reef.runtime.yarn.client;
 
 import org.apache.reef.client.DriverConfiguration;
@@ -14,44 +32,43 @@ import org.junit.Test;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Tests the local yarn client configuration.
+ */
 public class TestLocalYarnClientConfiguration {
 
-    private final static Logger LOG = Logger.getLogger(TestLocalYarnClientConfiguration.class.getName());
+  private static final Logger LOG = Logger.getLogger(TestLocalYarnClientConfiguration.class.getName());
 
-    @Test
-    public void testLocalYarnClientConfiguration() throws Exception {
-        final Configuration RUNTIME_CONF = LocalYarnClientConfiguration.CONF
-                .set(LocalYarnClientConfiguration.ROOT_FOLDER, "C:\\Users\\tcondie\\")
-                .build();
+  @Test
+  public void testLocalYarnClientConfiguration() throws Exception {
+    final Configuration runtimeConf = LocalYarnClientConfiguration.CONF
+        .set(LocalYarnClientConfiguration.ROOT_FOLDER, "C:\\Users\\tcondie\\")
+        .build();
 
-        final Configuration DRIVER_CONF = DriverConfiguration.CONF
-                .set(DriverConfiguration.DRIVER_IDENTIFIER, "test")
-                .set(DriverConfiguration.GLOBAL_LIBRARIES, EnvironmentUtils.getClassLocation(TestDriver.class))
-                .set(DriverConfiguration.ON_DRIVER_STARTED, TestDriver.StartHandler.class)
-                .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, TestDriver.EvaluatorAllocatedHandler.class)
-                .set(DriverConfiguration.DRIVER_JOB_SUBMISSION_DIRECTORY, "/tmp")
-                .build();
-        final LauncherStatus status = DriverLauncher.getLauncher(RUNTIME_CONF).run(DRIVER_CONF, 0);
-        LOG.log(Level.INFO, "Launch status: " + status.toString());
+    final Configuration driverConf = DriverConfiguration.CONF
+        .set(DriverConfiguration.DRIVER_IDENTIFIER, "test")
+        .set(DriverConfiguration.GLOBAL_LIBRARIES, EnvironmentUtils.getClassLocation(TestDriver.class))
+        .set(DriverConfiguration.ON_DRIVER_STARTED, TestDriver.StartHandler.class)
+        .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, TestDriver.EvaluatorAllocatedHandler.class)
+        .set(DriverConfiguration.DRIVER_JOB_SUBMISSION_DIRECTORY, "/tmp")
+        .build();
+    final LauncherStatus status = DriverLauncher.getLauncher(runtimeConf).run(driverConf, 0);
+    LOG.log(Level.INFO, "Launch status: " + status.toString());
+  }
+
+  @Unit
+  private static final class TestDriver {
+
+    final class StartHandler implements EventHandler<StartTime> {
+      @Override
+      public void onNext(final StartTime value) {
+      }
     }
 
-
-
-    @Unit
-    private final static class TestDriver {
-
-        final class StartHandler implements EventHandler<StartTime> {
-            @Override
-            public void onNext(StartTime value) {
-
-            }
-        }
-
-        final class EvaluatorAllocatedHandler implements EventHandler<AllocatedEvaluator> {
-            @Override
-            public void onNext(AllocatedEvaluator value) {
-
-            }
-        }
+    final class EvaluatorAllocatedHandler implements EventHandler<AllocatedEvaluator> {
+      @Override
+      public void onNext(final AllocatedEvaluator value) {
+      }
     }
+  }
 }
